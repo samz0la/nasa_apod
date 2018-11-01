52a9ab34739d44f3ae80f7243ecf334e6ad20382
samuelandazola <szola3@icloud.com>
2018-11-01 09:04:49 -0600

    Initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/MainActivity.java
@@ -0,0 +1,59 @@
+package edu.cnm.deepdive.nasaapod;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+  private static final String DATE_FORMAT = "yyyy-MM-dd";
+  private static final String CALENDAR_KEY = "calendar";
+  private static final String APOD_KEY = "apod";
+
+  private WebView webView;
+  private String apiKey;
+  private ProgressBar progressSpinner;
+  private FloatingActionButton jumpDate;
+  private Calendar calendar;
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    setUpWebView();
+    // TODO Setup service
+    // TODO Setup UI
+    // TODO Setup defaults
+  }
+
+  private void setUpWebView() {
+    webView = findViewById(R.id.web_view);
+    webView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        return false;
+      }
+
+      @Override
+      public void onPageFinished(WebView view, String url) {
+        progressSpinner.setVisibility(View.GONE);
+        //TODO Display toast with APOD item title.
+      }
+    });
+    WebSettings settings = webView.getSettings();
+    settings.setJavaScriptEnabled(true);
+    settings.setSupportZoom(true);
+    settings.setBuiltInZoomControls(true);
+    settings.setDisplayZoomControls(false);
+    settings.setUseWideViewPort(true);
+    settings.setLoadWithOverviewMode(true);
+  }
+
+}
