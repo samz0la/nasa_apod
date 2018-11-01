1ede50c087d2ad0d11a1d228d1fcdf7e6e1f52c4
samuelandazola <szola3@icloud.com>
2018-11-01 12:05:23 -0600

    Finished retrieving data from NASA Working except for orientation change.

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.nasaapod.controller;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +12,20 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
 import edu.cnm.deepdive.nasaapod.R;
+import edu.cnm.deepdive.nasaapod.controller.DateTimePickerFragment.Mode;
 import edu.cnm.deepdive.nasaapod.model.Apod;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -42,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     setUpWebView();
     setupService();
     setupUI();
-    // TODO Setup defaults
+    setupDefaults(savedInstanceState);
   }
 
   private void setUpWebView() {
@@ -56,7 +65,9 @@ public class MainActivity extends AppCompatActivity {
       @Override
       public void onPageFinished(WebView view, String url) {
         progressSpinner.setVisibility(View.GONE);
-        //TODO Display toast with APOD item title.
+        if(apod != null) {
+          Toast.makeText(MainActivity.this, apod.getTitle(), Toast.LENGTH_LONG).show();
+        }
       }
     });
     WebSettings settings = webView.getSettings();
@@ -72,13 +83,7 @@ public class MainActivity extends AppCompatActivity {
     progressSpinner = findViewById(R.id.progress_spinner);
     progressSpinner.setVisibility(View.GONE);
     jumpDate = findViewById(R.id.jump_date);
-    jumpDate.setOnClickListener(new OnClickListener() {
-      //TODO Use lambda form.
-      @Override
-      public void onClick(View v) {
-        //TODO Display date picker
-      }
-    });
+    jumpDate.setOnClickListener(v -> pickDate());
   }
   private void setupService() {
     Gson gson = new GsonBuilder()
@@ -91,5 +96,66 @@ public class MainActivity extends AppCompatActivity {
         .build();
     service = retrofit.create(ApodService.class);
     apiKey = BuildConfig.API_KEY;
+  }
+
+  private void setupDefaults(Bundle savedInstanceState) {
+    calendar = Calendar.getInstance();
+// TODO Check for savedInstanceState
+    new ApodTask().execute();
+  }
+
+  private void pickDate() {
+    DateTimePickerFragment picker = new DateTimePickerFragment();
+    picker.setMode(Mode.DATE);
+    picker.setCalendar(calendar);
+    picker.setListener((cal) -> new ApodTask().execute(cal.getTime()));
+    picker.show(getSupportFragmentManager(), picker.getClass().getSimpleName());
+  }
+
+  private class ApodTask extends AsyncTask<Date, Void, Apod> {
+
+    private Date date;
+
+
+    @Override
+    protected void onPreExecute() {
+      progressSpinner.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onPostExecute(Apod apod) {
+       MainActivity.this.apod = apod;
+       //TODO Handle hdUrl
+       webView.loadUrl(apod.getUrl());
+    }
+
+    @Override
+    protected void onCancelled(Apod apod) {
+      Context context = MainActivity.this;
+      progressSpinner.setVisibility(View.GONE);
+      Toast.makeText(context, R.string.error_message, Toast.LENGTH_LONG)
+          .show();
+    }
+
+    @Override
+    protected Apod doInBackground(Date... dates) {
+      Apod apod = null;
+      try {
+        DateFormat format = new SimpleDateFormat(DATE_FORMAT);
+        date = (dates.length == 0) ? calendar.getTime() : dates[0];
+        Response<Apod> response = service.get(apiKey, format.format(date)).execute();
+        if (response.isSuccessful()) {
+          apod = response.body();
+          calendar.setTime(date);
+        }
+      } catch (IOException e) {
+        // Do nothing: apod is already null.
+      } finally {
+        if(apod == null) {
+          cancel(true);
+        }
+      }
+      return apod;
+    }
   }
 }
