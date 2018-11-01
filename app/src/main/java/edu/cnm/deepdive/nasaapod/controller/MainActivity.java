fc9f28ee0a4744e93238768a68b971ad69161b03
samuelandazola <szola3@icloud.com>
2018-11-01 10:07:17 -0600

    Defined service & model

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -1,15 +1,24 @@
-package edu.cnm.deepdive.nasaapod;
+package edu.cnm.deepdive.nasaapod.controller;
 
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import edu.cnm.deepdive.nasaapod.BuildConfig;
+import edu.cnm.deepdive.nasaapod.R;
+import edu.cnm.deepdive.nasaapod.model.Apod;
+import edu.cnm.deepdive.nasaapod.service.ApodService;
 import java.util.Calendar;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +32,16 @@ public class MainActivity extends AppCompatActivity {
   private ProgressBar progressSpinner;
   private FloatingActionButton jumpDate;
   private Calendar calendar;
+  private ApodService service;
+  private Apod apod;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setUpWebView();
-    // TODO Setup service
-    // TODO Setup UI
+    setupService();
+    setupUI();
     // TODO Setup defaults
   }
 
@@ -56,4 +68,28 @@ public class MainActivity extends AppCompatActivity {
     settings.setLoadWithOverviewMode(true);
   }
 
+  private void setupUI() {
+    progressSpinner = findViewById(R.id.progress_spinner);
+    progressSpinner.setVisibility(View.GONE);
+    jumpDate = findViewById(R.id.jump_date);
+    jumpDate.setOnClickListener(new OnClickListener() {
+      //TODO Use lambda form.
+      @Override
+      public void onClick(View v) {
+        //TODO Display date picker
+      }
+    });
+  }
+  private void setupService() {
+    Gson gson = new GsonBuilder()
+        .excludeFieldsWithoutExposeAnnotation()
+        .setDateFormat(DATE_FORMAT)
+        .create();
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(getString(R.string.base_url))
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build();
+    service = retrofit.create(ApodService.class);
+    apiKey = BuildConfig.API_KEY;
+  }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/model/Apod.java
@@ -0,0 +1,128 @@
+package edu.cnm.deepdive.nasaapod.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import java.util.Date;
+
+public class Apod implements Parcelable {
+
+  public static final Creator CREATOR = new Creator();
+
+  @Expose
+  private Date date;
+
+  @Expose
+  private String title;
+
+  @Expose
+  private String explanation;
+
+  @Expose
+  private String copyright;
+
+  @Expose
+  private String url;
+
+  @Expose
+  @SerializedName("media_type")
+  private String mediaType;
+
+  @Expose
+  @SerializedName("hdurl")
+  private String hdUrl;
+
+  @Expose
+  @SerializedName("service_version")
+  private String serviceVersion;
+
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getExplanation() {
+    return explanation;
+  }
+
+  public void setExplanation(String explanation) {
+    this.explanation = explanation;
+  }
+
+  public String getCopyright() {
+    return copyright;
+  }
+
+  public void setCopyright(String copyright) {
+    this.copyright = copyright;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getMediaType() {
+    return mediaType;
+  }
+
+  public void setMediaType(String mediaType) {
+    this.mediaType = mediaType;
+  }
+
+  public String getHdUrl() {
+    return hdUrl;
+  }
+
+  public void setHdUrl(String hdUrl) {
+    this.hdUrl = hdUrl;
+  }
+
+  public String getServiceVersion() {
+    return serviceVersion;
+  }
+
+  public void setServiceVersion(String serviceVersion) {
+    this.serviceVersion = serviceVersion;
+  }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    //TODO Write fields to destination.
+
+  }
+
+  private static class Creator implements Parcelable.Creator<Apod> {
+
+    @Override
+    public Apod createFromParcel(Parcel source) {
+      //TODO Create Apod object & populate its fields from source.
+      return null;
+    }
+
+    @Override
+    public Apod[] newArray(int size) {
+      return new Apod[size];
+    }
+  }
+}
